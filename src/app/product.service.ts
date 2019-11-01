8f8886bf9fd34bff4d6b06c83255ba867033dc9a
Ismaeel Shuaib Alfani <39034490+ismaeelSh@users.noreply.github.com>
2019-11-01 02:36:11 +0000

    Displaying List of Produt commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,7 +14,7 @@ import { Product } from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _productUrl = '../assets/products.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -23,6 +23,6 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productUrl).map(response => <Product[]>response.json());
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
